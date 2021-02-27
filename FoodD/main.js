c7699ab6ad053cd97c1b1659d099c2ac648b950b
Võ Hoài Nam <vhnvohoainam@gmail.com>
2021-02-27 12:50:00 +0700

    Hoan thien

--- FoodD/main.js
+++ FoodD/main.js
@@ -7,6 +7,20 @@ window.addEventListener('DOMContentLoaded', () => {
         else buttondown.style.visibility="visible";
     })
     /* End scroll to top button */
-    
+
+    /* Mobile */
+    showmenuitem=document.querySelector('.show-menu-item');
+    menuitem=document.querySelector('.menu-item')
+    console.log(menuitem);
+    showmenuitem.onclick=()=>{
+        menuitem.classList.toggle("show-element");
+    }
+
+    showmenu=document.querySelector('.fa-bars');
+    menu=document.querySelector('.mobile-menu');
+    showmenu.onclick=()=>{
+        menu.classList.toggle("show-element");
+    }
+    /* End Mobile */
 });
 
